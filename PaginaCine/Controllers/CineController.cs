56c4f597a49427a60247b1846ee5c27b0f8b6f8e
RobertoRamiez <71281862+RobertoRamiez@users.noreply.github.com>
2020-11-19 19:27:57 -0800

    Actualizacion

--- PaginaCine/Controllers/CineController.cs
+++ PaginaCine/Controllers/CineController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace PaginaCine.Controllers
 {
@@ -17,10 +18,40 @@ namespace PaginaCine.Controllers
             return View(cines);
         }
 
+        public ActionResult Details(int id)
+        {
+            var category = db.Categories.Find(id);
+            return View(category);
+        }
+
         public ActionResult Delete(int id)
         {
             var cine = db.Cines.Find(id);
             return View(cine);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var category = db.Categories.Find(id);
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, Category c)
+        {
+            try
+            {
+                var category = db.Categories.Find(id);
+
+                category.Name = c.Name;
+                category.Description = c.Description;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
